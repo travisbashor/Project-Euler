79df32f2a6044c537f241410faf4c527bad89569
Travis Bashor <travis.bashor@gmail.com>
2014-10-22 18:07:39 -0400

    comment Euler 4

--- euler4.py
+++ euler4.py
@@ -1,13 +1,17 @@
 # bottom- and top-end of the range of products of two 3-digit values
 low, high = 100 * 100, 999 * 999
 
+# returns a number with its digits in reverse order
 def reverse(number):
   return int(str(number)[::-1])
 
+# uses the reverse() method to determine whether or not the number is palindromic
 def is_palindrome(number):
   return number == reverse(number)
 
+# boolean-returning, on whether this number is the product of two 3-digit integers
 def is_good_product(number):
+  # use the root to narrow the search for factors
   root = number**0.5
 
   # check here first, since we are starting at the top end
@@ -23,9 +27,11 @@ def is_good_product(number):
         return True
   return False
 
+# given the highest- and lowest-possible products, find the highest palindromic product of two 3-digit numbers
 def findPalindrome(top, bottom):
   for i in range(top, bottom, -1):
     if (is_palindrome(i) and is_good_product(i)):
       return str(i)
 
+# show answer
 print("The highest palindromic product of two three-digit numbers is: ", findPalindrome(high, low))
